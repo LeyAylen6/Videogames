54344b22b0212e60f28817bb319ac789515ea3ea
Leila <leiisalguero@gmaill.com>
2023-05-16 14:01:38 -0300

    add component for errors and messages

--- client/src/App.js
+++ client/src/App.js
@@ -4,21 +4,32 @@ import Landing from './components/Landing/Landing';
 import Home from './components/Home/Home';
 import Form from './components/CreateNewGame/Form';
 import Detail from './components/Detail/Detail';
+import MyGames from './components/MyGames/MyGames';
 import { useLocation } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
+import { useSelector } from 'react-redux';
+import Error from './components/Error/Error';
 
 function App() {
+  const message = useSelector(state => state.message)
+
   return (
     <div className="App">
-      {useLocation().pathname !== '/' ? <NavBar /> : null}
+      
+      {message ? <Error error={message} /> : null}
 
-      <Routes>
-        <Route path='/' element={<Landing />} />
-        <Route path='/home' element={<Home />} />
-        <Route path='/create' element={<Form />} />
-        <Route path='/detail/:id' element={<Detail />} /> 
-      </Routes> 
+      <div>
+        {useLocation().pathname !== '/' ? <NavBar /> : null}
+        <Routes>
+          <Route path='/' element={<Landing />} />
+          <Route path='/home' element={<Home />} />
+          <Route path='/create' element={<Form />} />
+          <Route path='/detail/:id' element={<Detail />} /> 
+          <Route path='/mygames' element={<MyGames />} /> 
+        </Routes> 
+      </div>
     </div>
+
   );
 }
 
---
+++ client/src/components/Error/Error.jsx
@@ -0,0 +1,22 @@
+import { useDispatch } from 'react-redux'
+import styles from './../../components/Error/error.module.css'
+import { clear } from '../../redux/action';
+
+const Error = (props) => {
+    const dispatch = useDispatch();
+
+    const onClick = () => {
+        clear(dispatch)
+    }
+
+    return (
+        <div className={styles.background}>
+            <div className={styles.error}>
+                <h1 className={styles.message}>{props.error}</h1>
+                <button className={styles.delete} onClick={onClick}>X</button>
+            </div>
+        </div>
+    );
+}
+
+export default Error;
