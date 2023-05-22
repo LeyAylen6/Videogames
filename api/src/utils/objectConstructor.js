36000a0df73a83777f7614981c3e08cd8551f61a
Leila <leiisalguero@gmaill.com>
2023-05-22 14:49:14 -0300

    add changes for deploy

--- api/src/utils/objectConstructor.js
+++ api/src/utils/objectConstructor.js
@@ -1,14 +1,14 @@
 const objectConstructor = (gameData) => {
     let myGenres = gameData.genres || gameData.Genres
     let myPlatforms = Array.isArray(gameData.platforms) ? gameData.platforms.map(platform => platform.platform) : [{ name: gameData.platforms}]
-
+    
     return {
         id: gameData.id, 
         name: gameData.name,
         description: gameData.description ? gameData.description : null, 
         platforms: myPlatforms,
         image: gameData.background_image || gameData.image, 
-        releaseDate: gameData.released || gameData.releaseDate.toLocaleDateString('en-CA'),
+        releaseDate: gameData?.releaseDate?.toLocaleDateString('en-CA') || gameData.released,
         rating: gameData.rating,
         genres: myGenres,
     }
--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,9 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './redux/store';
+import axios from 'axios';
 
+axios.defaults.baseURL = 'http://localhost:3001'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
